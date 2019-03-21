2a1544f7b81739d35ad74308e53c70dea49dee4d
Sandipan Dutta <43114236+sandipandutta21@users.noreply.github.com>
2019-03-21 17:20:22 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,29 +1,28 @@
-from PIL import Image
-import numpy as np
-from pylab import * 
+from PIL import Image #importing required libs
+import numpy as np 
+
 #encryption
 img1 = (Image.open('image.jpeg').convert('L')) 
-img1.show()
+img1.show()   #showing the image that will be encrypted
 
-img = array((Image.open('image.jpeg').convert('L')))
-img16 = np.array(img, dtype=np.uint32)
+img = array((Image.open('image.jpeg').convert('L'))) # converting the RGB image to greyscale  using the .convert() and 'L' aurgument
+img32 = np.array(img, dtype=np.uint32) #converting the uint8 type numpy.ndarray to uint32 type
 #img = Image.fromarray(img)
 #img.save('img.bmp')
-#img = array((Image.open('img.bmp').convert('i')))
-a,b = img.shape
+a,b = img.shape #counting the no of rows and cols
 print('\n\nOriginal image: ')
-print(img16)
+print(img32)
 print((a,b))
-tup = a,b
+tup = a,b # store the number of rows and cols into a tuple
 
-for i in range (0, tup[0]):
-	for j in range (0, tup[1]):
-		x = img16[i][j] 
-		x = (pow(x,3)%25777)
-		img16[i][j] = x
+for i in range (0, tup[0]): #going through row 0 to the last row of the matrix 
+	for j in range (0, tup[1]): #going through col 0 to the last col of the matrix
+		x = img32[i][j] #storing the value of the element into a temp variable
+		x = (pow(x,3)%25777) #applying rsa keys 
+		img32[i][j] = x #storing back the values to the original matrix
 print('\n\nEncrypted image: ')
-print(img16)
-imgOut = Image.fromarray(img16)
+print(img32)
+imgOut = Image.fromarray(img32) #making an image from the matrix
 imgOut.show()
 ##imgOut.save('img.bmp')
 
@@ -31,24 +30,23 @@ imgOut.show()
 
 ##img2 = (Image.open('img.bmp').convert('L'))
 ##img2.show()
-img3_16 = img16
-img3_16 = np.array(img, dtype=np.uint32)
+img3_32 = img32
+img3_32 = np.array(img, dtype=np.uint32)
 print('\n\nEncrypted image: ')
-print(img3_16)
-a1,b1 = img3_16.shape
+print(img3_32)
+a1,b1 = img3_32.shape
 print((a1,b1))
 tup1 = a1,b1
 
 for i1 in range (0, tup1[0]):
 	 for j1 in range (0, tup1[1]):
-		 x1 = img3_16[i1][j1] 
+		 x1 = img3_32[i1][j1] 
 		 x1 = (pow(x,16971)%25777)	
-		 img3_16[i][j] = x1
+		 img3_32[i][j] = x1
 		 #img3[i1][j1]= (img3[i1][j1] % 256)
 print('\n\nDecrypted image: ')
-print(img3_16)
-imgOut1 = Image.fromarray(img3_16)
+print(img3_32)
+imgOut1 = Image.fromarray(img3_32)
 imgOut1.show()
-print(type(img3_16))
 #imgOut1.save('img1.bmp')
 
